d34c0f265cf63d5bd5fb8e18e5af57ada40037f4
LeonardoC1302 <lecespedes@estudiantec.cr>
2023-06-04 15:17:34 -0600

    Report

--- preliminar2/queryReport.sql
+++ preliminar2/queryReport.sql
@@ -1,24 +1,26 @@
-SELECT
-	ad.countryId,
-	a.producerId,
-	w.wasteType,
-	SUM(wxc.volume) AS TotalCollected,
-	SUM(p.processCost) AS TotalCost,
-	SUM(p.processPrice) AS TotalCharge,
-	SUM(p.processPrice - p.processCost) AS NetProfit
-FROM
-	collectionAssignments a
-	JOIN collectionPoints cp ON a.collectionPointId = cp.collectionPointId
-	JOIN addresses ad ON cp.addressId = ad.addressId
-	JOIN collections c ON c.collectionAssignmentId = a.collectionAssignmentId
-	JOIN wastesXcollections wxc ON wxc.collectionId = c.collectionId
-	JOIN wastes w ON w.wasteId = wxc.wasteId
-	JOIN processes p ON p.wasteTypeId = w.wasteType
-WHERE
-	a.collectionDate BETWEEN @startDate AND @endDate AND ad.countryId = @countryId
-GROUP BY
-	ad.countryId,
-	a.producerId,
-	w.wasteType
-ORDER BY
-	w.wasteType ASC
+	SELECT
+		ad.countryId,
+		i.industryTypeName,
+		w.wasteType,
+		SUM(wxc.volume) AS TotalCollected,
+		SUM(p.processCost) AS TotalCost,
+		SUM(p.processPrice) AS TotalCharge,
+		SUM(p.processPrice - p.processCost) AS NetProfit
+	FROM
+		collectionAssignments a
+		JOIN collectionPoints cp ON a.collectionPointId = cp.collectionPointId
+		JOIN addresses ad ON cp.addressId = ad.addressId
+		JOIN collections c ON c.collectionAssignmentId = a.collectionAssignmentId
+		JOIN wastesXcollections wxc ON wxc.collectionId = c.collectionId
+		JOIN wastes w ON w.wasteId = wxc.wasteId
+		JOIN processes p ON p.wasteTypeId = w.wasteType
+		JOIN producers pr ON pr.producerId = a.producerId
+		JOIN industryTypes i ON i.industryTypeId = pr.industryTypeId
+	WHERE
+		a.collectionDate BETWEEN @startDate AND @endDate AND ad.countryId = @countryId
+	GROUP BY
+		ad.countryId,
+		i.industryTypeName,
+		w.wasteType
+	ORDER BY
+		w.wasteType ASC
